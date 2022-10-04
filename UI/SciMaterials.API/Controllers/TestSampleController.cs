c902b9a46340dbafa5c5607a9ed26cb7fab07b25
Tarxos <ftarxos@gmail.com>
2022-10-04 16:40:10 +0300

    add base test sample for files api tests

---
+++ UI/SciMaterials.API/Controllers/TestSampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SciMaterials.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestSampleController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Check()
+    {
+        return Ok();
+    }
+}
