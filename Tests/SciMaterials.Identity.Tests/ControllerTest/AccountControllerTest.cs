0c83f4d02cf77c7c1f12ca95e93d91e51042f3dc
AlexanderMeshchaninov <alexander_freeman85@mail.ru>
2022-11-14 21:34:10 +0300

    - Added CreateRole test; - Added ConfirmEmail test;

--- Tests/SciMaterials.Identity.Tests/ControllerTest/AccountControllerTest.cs
+++ Tests/SciMaterials.Identity.Tests/ControllerTest/AccountControllerTest.cs
@@ -1,17 +1,20 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using HttpContextMoq;
 using HttpContextMoq.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SciMaterials.Contracts.API.Constants;
 using SciMaterials.Contracts.Auth;
 using SciMaterials.Contracts.Enums;
+using SciMaterials.Contracts.Identity.API.DTO.Roles;
 using SciMaterials.Contracts.Identity.API.DTO.Users;
+using SciMaterials.Contracts.Identity.Clients.Clients.Responses.DTO;
+using SciMaterials.Contracts.Identity.Clients.Clients.Responses.Roles;
 using SciMaterials.Contracts.Identity.Clients.Clients.Responses.User;
 using SciMaterials.Identity.Tests.MockServices;
 using SciMaterials.UI.MVC.Identity.Controllers;
@@ -312,7 +315,7 @@ namespace SciMaterials.Identity.Tests.ControllerTest
         }
         
         [Fact]
-        public async Task GetRefreshTokenAsync_Return_StatusCode200WithResult()
+        public async Task GetRefreshTokenAsync_Return_StatusCode200_And_Result()
         {
             //Arrage
             const string expected_urlAddress = "http://localhost:5185/auth/refresh_token";
@@ -384,26 +387,162 @@ namespace SciMaterials.Identity.Tests.ControllerTest
                 It.Is<IdentityUser>(s => s.NormalizedEmail.Equals(expected_email.ToUpper())), 
                 It.IsAny<List<string>>()));
         }
-        //
-        // [Fact]
-        // public async Task ConfirmEmailAsync_Return_StatusCode200WithResult()
-        // {
-        //     //Arrage
-        //
-        //     //Act
-        //
-        //     //Assert
-        // }
-        //
-        // [Fact]
-        // public async Task CreateRoleAsync_Return_StatusCode200WithResult()
-        // {
-        // }
-        //
-        // [Fact]
-        // public async Task GetAllRolesAsync_Return_StatusCode200WithResult()
-        // {
-        // }
+        
+        [Fact]
+        public async Task ConfirmEmailAsync_Return_StatusCode200_And_Result()
+        {
+            //Arrage
+            const string expected_id = "identificationNumber";
+            const string expected_confirmToken = "confirmToken";
+            const string expected_email = "pupkin@mail.ru";
+            const string expected_name = "pupkin@mail.ru";
+            const string expected_concurrencystamp = "concurrencyStamp";
+            const string expected_securitystamp = "securityStamp";
+            const string expected_message = $"Учетная запись {expected_email} успешно подтверждена";
+
+            var identity_user = new IdentityUser()
+            {
+                Id = expected_id,
+                UserName = expected_name,
+                NormalizedUserName = expected_name.ToUpper(),
+                NormalizedEmail = expected_email.ToUpper(),
+                Email = expected_email,
+                EmailConfirmed = true,
+                ConcurrencyStamp = expected_concurrencystamp,
+                SecurityStamp = expected_securitystamp
+            };
+            
+            _UserManagerMock.Setup(x => x.FindByIdAsync(
+                It.Is<string>(s => s.Equals(expected_id)))).ReturnsAsync(identity_user).Verifiable();
+
+            var identity_result = new IdentityResultMock(true);
+            _UserManagerMock.Setup(x => x.ConfirmEmailAsync(
+                identity_user, expected_confirmToken)).ReturnsAsync(identity_result).Verifiable();
+            
+            //Act
+            var result = await _AccountController.ConfirmEmailAsync(expected_id, expected_confirmToken);
+            var actionResultObj = Assert.IsType<OkObjectResult>(result);
+            var okResult = Assert.IsAssignableFrom<ClientConfirmEmailResponse>(actionResultObj.Value);
+            
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, actionResultObj.StatusCode);
+            Assert.Equal((int)ResultCodes.Ok, okResult.Code);
+            Assert.True(okResult.Succeeded);
+            Assert.Equal(expected_message, okResult.Message);
+            
+            //*-----------------------------------------------------*//
+            _UserManagerMock.Verify(x => x.FindByIdAsync(It.Is<string>(s => s.Equals(expected_id))));
+            _UserManagerMock.Verify(x => x.ConfirmEmailAsync(
+                It.Is<IdentityUser>(s => s.Email.Equals(identity_user.Email)), 
+                It.Is<string>(s => s.Equals(expected_confirmToken))));
+        }
+        
+        [Fact]
+        public async Task CreateRoleAsync_Return_StatusCode200_And_Result()
+        {
+            //Arrage
+            const string expected_urlAddress = "http://localhost:5185/auth/create_role";
+            const string expected_email = "sa@mail.ru";
+            const string expected_name = "sa@mail.ru";
+            const string expected_role = "manager";
+            
+            var identity = new ClaimsIdentity();
+            identity.AddClaims(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, expected_email),
+                new Claim(ClaimTypes.Name, expected_name),
+                new Claim(ClaimTypes.Role, expected_role)
+            });
+            var user = new ClaimsPrincipal(identity);
+            
+            _HttpContextMock = new HttpContextMock();
+            _HttpContextMock.User = user;
+            _HttpContextMock.SetupUrl(expected_urlAddress);
+            _ContextAccessorMock.Setup(x => x.HttpContext).Returns(_HttpContextMock);
+            _AccountController.ControllerContext.HttpContext = _HttpContextMock;
+
+            var request = new CreateRoleRequest()
+            {
+                RoleName = expected_role,
+            };
+            
+            const string expected_message = $"Роль {expected_role} успешно добавлена в систему";
+
+            var identity_role = new IdentityRole(request.RoleName.ToLower());
+            var identity_result = new IdentityResultMock(true);
+            _RoleManagerMock.Setup(x => x.CreateAsync(
+                It.Is<IdentityRole>(s => s.Name.Equals(identity_role.Name))))
+                .ReturnsAsync(identity_result).Verifiable();
+            
+            //Act
+            var result = await _AccountController.CreateRoleAsync(request);
+            var actionResultObj = Assert.IsType<OkObjectResult>(result);
+            var okResult = Assert.IsAssignableFrom<ClientCreateRoleResponse>(actionResultObj.Value);
+            
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, actionResultObj.StatusCode);
+            Assert.Equal((int)ResultCodes.Ok, okResult.Code);
+            Assert.True(okResult.Succeeded);
+            Assert.Equal(expected_message, okResult.Message);
+            
+            //*-----------------------------------------------------*//
+            _RoleManagerMock.Verify(x => x.CreateAsync(It.Is<IdentityRole>(s => s.Name.Equals(identity_role.Name))));
+        }
+
+        [Fact]
+        public async Task GetAllRolesAsync_Return_StatusCode200WithResult()
+        {
+            //Arrage
+            const string expected_urlAddress = "http://localhost:5185/auth/get_all_roles";
+            const string expected_email = "sa@mail.ru";
+            const string expected_name = "sa@mail.ru";
+            const string expected_role_one = "admin";
+            const string expected_role_two = "user";
+            const string expected_role_one_id = "id001";
+            const string expected_role_two_id = "id002";
+
+            var identity = new ClaimsIdentity();
+            identity.AddClaims(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, expected_email), new Claim(ClaimTypes.Name, expected_name),
+                new Claim(ClaimTypes.Role, "admin")
+            });
+            var user = new ClaimsPrincipal(identity);
+
+            _HttpContextMock = new HttpContextMock();
+            _HttpContextMock.User = user;
+            _HttpContextMock.SetupUrl(expected_urlAddress);
+            _ContextAccessorMock.Setup(x => x.HttpContext).Returns(_HttpContextMock);
+            _AccountController.ControllerContext.HttpContext = _HttpContextMock;
+
+            var identity_roles_list = new List<IdentityRole>()
+            {
+                new IdentityRole(){Id = expected_role_one_id, Name = expected_role_one, NormalizedName = expected_role_one.ToUpper()},
+                new IdentityRole(){Id = expected_role_two_id, Name = expected_role_two, NormalizedName = expected_role_two.ToUpper()}
+            }.AsQueryable();
+            
+            //TODO: Есть проблема с ToListAsync... Буду думать...
+            _RoleManagerMock.Setup(x => x.Roles)
+                .Returns(identity_roles_list);
+            
+            var auth_roles_list = new List<AuthRoles>();
+             foreach (var role in identity_roles_list)
+                auth_roles_list.Add(new AuthRoles() { RoleName = role.Name, Id = role.Id });
+
+            //Act
+            var result = await _AccountController.GetAllRolesAsync();
+            var actionResultObj = Assert.IsType<OkObjectResult>(result);
+            var okResult = Assert.IsAssignableFrom<ClientGetAllRolesResponse>(actionResultObj.Value);
+            
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, actionResultObj.StatusCode);
+            Assert.Equal((int)ResultCodes.Ok, okResult.Code);
+            Assert.True(okResult.Succeeded);
+            Assert.Equal(auth_roles_list, okResult.Roles);
+        }
         //
         // [Fact]
         // public async Task GetRoleByIdAsync_Return_StatusCode200WithResult()
--- Tests/SciMaterials.Identity.Tests/MockServices/RoleManagerMock.cs
+++ Tests/SciMaterials.Identity.Tests/MockServices/RoleManagerMock.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-
 using Moq;
 
 namespace SciMaterials.Identity.Tests.MockServices
