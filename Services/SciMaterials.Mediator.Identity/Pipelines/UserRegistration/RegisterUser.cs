5fde0f75fbaa8806b5baf91fb627e356d1368ebf
Tarxos <ftarxos@gmail.com>
2022-12-23 20:30:47 +0300

    inject mediator, and create RegisterUser mediator handler

---
+++ Services/SciMaterials.Mediator.Identity/Pipelines/UserRegistration/RegisterUser.cs
@@ -0,0 +1,17 @@
+﻿using Mediator;
+using SciMaterials.Contracts.Identity.API.Requests.Users;
+using SciMaterials.Contracts.Identity.API.Responses.User;
+using SciMaterials.Contracts.Result;
+
+namespace SciMaterials.Mediator.Identity.Pipelines.UserRegistration;
+
+public sealed record RegisterUser(RegisterRequest Request) : IRequest<Result<RegisterUserResponse>>;
+
+
+public sealed class RegisterUserHandler : IRequestHandler<RegisterUser, Result<RegisterUserResponse>>
+{
+    public async ValueTask<Result<RegisterUserResponse>> Handle(RegisterUser request, CancellationToken cancellationToken)
+    {
+        return null;
+    }
+}
