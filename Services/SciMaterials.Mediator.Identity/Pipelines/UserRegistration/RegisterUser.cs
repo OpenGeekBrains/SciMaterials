806c4f536406530d2f687b235d3ee86fb4875838
Tarxos <ftarxos@gmail.com>
2022-12-26 23:58:59 +0300

    prototype RegisterUser pipeline with mediator

--- Services/SciMaterials.Mediator.Identity/Pipelines/UserRegistration/RegisterUser.cs
+++ Services/SciMaterials.Mediator.Identity/Pipelines/UserRegistration/RegisterUser.cs
@@ -1,6 +1,8 @@
 ﻿using Mediator;
-using SciMaterials.Contracts.Identity.API.Requests.Users;
-using SciMaterials.Contracts.Identity.API.Responses.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using SciMaterials.Contracts;
+using SciMaterials.Contracts.Identity.API;
 using SciMaterials.Contracts.Result;
 
 namespace SciMaterials.Mediator.Identity.Pipelines.UserRegistration;
@@ -8,10 +10,65 @@ namespace SciMaterials.Mediator.Identity.Pipelines.UserRegistration;
 public sealed record RegisterUser(string Email, string UserName, string Password) : IRequest<Result>;
 
 
-public sealed class RegisterUserHandler : IRequestHandler<RegisterUser, Result<RegisterUserResponse>>
+public sealed class RegisterUserHandler : IRequestHandler<RegisterUser, Result>
 {
-    public async ValueTask<Result<RegisterUserResponse>> Handle(RegisterUser request, CancellationToken cancellationToken)
+    private readonly UserManager<IdentityUser> _UserManager;
+    private readonly ILogger<RegisterUserHandler> _Logger;
+    private readonly IMediator _Mediator;
+
+    public RegisterUserHandler(UserManager<IdentityUser> UserManager, ILogger<RegisterUserHandler> Logger, IMediator Mediator)
     {
-        return null;
+        _UserManager   = UserManager; 
+        _Logger        = Logger;
+        _Mediator = Mediator;
+    }
+
+    public async ValueTask<Result> Handle(RegisterUser Request, CancellationToken Cancel)
+    {
+        try
+        {
+            Cancel.ThrowIfCancellationRequested();
+
+            var identity_user   = new IdentityUser { Email = Request.Email, UserName = Request.UserName };
+            var identity_result = await _UserManager.CreateAsync(identity_user, Request.Password);
+            
+            if (!identity_result.Succeeded)
+            {
+                var errors = identity_result.Errors.Select(e => e.Description).ToArray();
+                _Logger.LogWarning(
+                    "Не удалось зарегистрировать пользователя {Email}: {errors}",
+                    Request.Email,
+                    string.Join(",", errors));
+
+                return Result.Failure(Errors.Identity.Register.Fail);
+            }
+
+            await _UserManager.AddToRoleAsync(identity_user, AuthApiRoles.User);
+            var email_confirm_token = await _UserManager.GenerateEmailConfirmationTokenAsync(identity_user);
+
+            var result = await _Mediator.Send(new RegisterUserInServices(identity_user.Id, Request.UserName, Request.Email), Cancel);
+            if (result.IsFaulted)
+            {
+                // Send notification to moderators to handle failure on registration
+                _Logger.LogWarning(
+                    "Не удалось зарегистрировать пользователя {Email} в сервисах: {ErrorCode}",
+                    Request.Email,
+                    result.Code);
+                return Result.Failure(Errors.Identity.Register.FailToRegisterInServices);
+            }
+
+            await _Mediator.Send(new SendConfirmationLinkToEmail(identity_user.Id, email_confirm_token), Cancel);
+
+            return Result.Success();
+        }
+        catch (OperationCanceledException)
+        {
+            return Result.Failure(Errors.App.OperationCanceled);
+        }
+        catch (Exception ex)
+        {
+            _Logger.LogError("Пользователя не удалось зарегистрировать {Ex}", ex);
+            return Result.Failure(Errors.Identity.Register.Unhandled);
+        }
     }
 }
---
+++ Services/SciMaterials.Mediator.Identity/Pipelines/UserRegistration/SendConfirmationLinkToEmail.cs
@@ -0,0 +1,13 @@
+﻿using Mediator;
+
+namespace SciMaterials.Mediator.Identity.Pipelines.UserRegistration;
+
+public sealed record SendConfirmationLinkToEmail(string UserId, string AccessToken) : IRequest;
+
+public sealed class SendConfirmationLinkToEmailHandler : IRequestHandler<SendConfirmationLinkToEmail, Unit>
+{
+    public async ValueTask<Unit> Handle(SendConfirmationLinkToEmail Request, CancellationToken Cancel)
+    {
+        return Unit.Value;
+    }
+}
